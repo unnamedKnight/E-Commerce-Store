435ba895badf5f382ba4521bcd6e78e5c82d2c5d
unnamedKnight <backfrontdev47@gmail.com>
2023-02-04 16:40:35 +0600

    Add models in store app

--- src/store/models.py
+++ src/store/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+
+class Product(models.Model):
+
+    title = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255, default='un-branded')
+    description = models.TextField(blank=True)
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=1000, decimal_places=2)
+    # image = models.ImageField(upload_to='images/')
+
+
+    class Meta:
+        verbose_name_plural = "products"
+
+    def __str__(self):
+        return self.title
+
+
