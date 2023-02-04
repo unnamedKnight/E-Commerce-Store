0745a5be7359f0e1485548211cfbc36697e2e1bc
unnamedKnight <backfrontdev47@gmail.com>
2023-02-04 16:15:59 +0600

    Add store app

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -34,12 +34,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps for this project
+    'store.apps.StoreConfig',
 ]
 
 MIDDLEWARE = [
