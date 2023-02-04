9b8a775eca357ae222121160859b057c2199deca
unnamedKnight <backfrontdev47@gmail.com>
2023-02-04 19:02:21 +0600

    Add Image Upload functionality in Product model

--- src/store/models.py
+++ src/store/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=1000, decimal_places=2)
-    # image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/')
 
 
     class Meta:
