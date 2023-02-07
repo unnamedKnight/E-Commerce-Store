7f28bf0a40a3661e627b4d7fd9cb58da16112a77
unnamedKnight <backfrontdev47@gmail.com>
2023-02-07 20:02:49 +0600

    Add Base temaplate and Navbar

--- src/store/admin.py
+++ src/store/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Category, Product
+
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Product)
+# admin.site.register(Category)
+# admin.site.register(Product)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # this method doesn't produce unique slugs
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
