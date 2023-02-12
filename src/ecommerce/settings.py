ebe8c0a44ea6bcfbc3578bab29b723d181cc776d
unnamedKnight <backfrontdev47@gmail.com>
2023-02-12 20:35:44 +0600

    Add Cart app

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # apps for this project
     "store.apps.StoreConfig",
+    "cart.apps.CartConfig",
 ]
 
 MIDDLEWARE = [
--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("store.urls")),
+    path("cart", include("cart.urls"))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
