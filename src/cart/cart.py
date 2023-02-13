927756ba8155fcf6213f309a09ab717408b319ce
unnamedKnight <backfrontdev47@gmail.com>
2023-02-13 23:31:00 +0600

    Implement AJAX for Adding items in Cart

--- src/cart/cart.py
+++ src/cart/cart.py
@@ -2,32 +2,35 @@ class Cart:
     def __init__(self, request):
         self.session = request.session
         # getting the session or obtaining existing session
-        cart = self.session.get("session_key")
-        # checking if session_key is available in the  request
+        cart = self.session.get("cart_item")
+        # checking if cart_item is available in the  request
         # in other words checking if the user has any cookie or session or
         # if she or he visiting the website for the first time then
-        # generate a session key for the new user
-        if "session_key" not in request.session:
-            # creating new session for the new user
-            cart = self.session["session_key"] = {}
+        # generate a cart_item for the new user in the session
+        if "cart_item" not in request.session:
+            cart = self.session["cart_item"] = {}
 
         self.cart = cart
 
     def add(self, product, product_qty):
         product_id = str(product.id)
         if product_id in self.cart:
-            # here self.cart is equivalent self.session.get("session_key")
-            # in other words self.cart itself is the session_key dictionary
+            # here self.cart is equivalent self.session.get("cart_item")
+            # in other words self.cart itself is the cart_item dictionary
             # if product is already in cart we will update the quantity of the product
             self.cart[product_id]["qty"] = product_qty
 
         else:
-            # otherwise we need to create a new dictionary within the cart /self.session.get("session_key") dictionary
+            # otherwise we need to create a new dictionary within the cart /self.session.get("cart_item") dictionary
             # with the product_id
             # and product_id dictionary contains the product price and quantity
             self.cart[product_id] = {"price": str(product.price), 'qty': product_qty}
 
         self.session.modified = True
+
+    def __len__(self):
+
+        return sum(item['qty'] for item in self.cart.values())
 
 
 # note: this is how we can get session data in django shell
@@ -40,5 +43,5 @@ class Cart:
 # >>> session_key = Session.objects.get(pk='vo8d449aela661rc7ylw493zpbjysccu')
 # >>> session_key.get_decoded()
 # note: the following code is the result
-# {'session_key': {}, '_auth_user_id': '1', '_auth_user_backend': 'django.contrib.auth.backends.ModelBackend', /
+# {'cart_item': {}, '_auth_user_id': '1', '_auth_user_backend': 'django.contrib.auth.backends.ModelBackend', /
 # '_auth_user_hash': '0de5763a758d34e5024d3b6ca65bd46f4aff9f9f6623f88adca360f50898413b'}
--- src/cart/views.py
+++ src/cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 
 from store.models import Product
 from .cart import Cart
@@ -7,17 +8,32 @@ from .cart import Cart
 
 
 def cart_summary(request):
-    return render(request, 'cart/cart-summary.html')
+    return render(request, "cart/cart-summary.html")
 
 
 def cart_add(request):
     cart = Cart(request)
-    if request.method == 'POST':
-        product_id = int(request.POST.get('product_id'))
-        product_quantity = int(request.POST.get('product_quantity'))
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("product_id"))
+        product_quantity = int(request.POST.get("product_quantity"))
         product = get_object_or_404(Product, id=product_id)
 
         cart.add(product=product, product_qty=product_quantity)
+
+        return JsonResponse(
+            {
+                "product_quantity": cart.__len__(),
+            }
+            # note: the alternative of doing the above operation
+            # response = JsonResponse(
+            #     {
+            #         "the product is called:": product.title,
+            #         "Product quantity is: ": product_quantity,
+            #     }
+            # )
+        )
+
+        # return response
 
 
 def cart_update(request):
